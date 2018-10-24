42a8bab1edf626dc46ee7485d1453ba88458b4c7
alexis nouvel <alexis.nouvel@neotys.com>
2018-10-24 11:53:54 +0200

    Use Microsoft.Data.OData 5.8.4

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b5_2b87e-e2a6-402e-874f-ef3f21d666ad")]
+[assembly: Guid("b5-2b87e-e2a6-402e-874f-ef3f21d666ad")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
